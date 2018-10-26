7ede26f818300df6cb76ff6676ff3e81803e68f3
William Dry <wpbdry@gmail.com>
2018-10-26 10:07:24 +0000

    improved code readability

--- pset2/crack/crack.c
+++ pset2/crack/crack.c
@@ -7,6 +7,36 @@
 int const max_char_count = 5;
 string const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
+int iterate(string current_parent_string, string original_hash, string salt);
+
+int main(int argc, string argv[])
+{
+    if (argc == 2)
+    {
+        //Put this in a seperate function when you understand pointers
+        //Get hash and salt
+        char salt[3];
+        string const hash = argv[1];
+        salt[0] = hash[0];
+        salt[1] = hash[1];
+        salt[2] = '\0';
+
+        //Pass to iteration: current parent string, hash, salt
+        if(iterate("", hash, salt))
+        {
+            //Print status if password not found. If password is found it is printed by the iterate function
+            printf("Password not found in string with length of up to %i characters\nusing the character set \"%s\"\n",
+            max_char_count, characters);
+        }
+        return 0;
+    }
+    else
+    {
+        printf("Error: please pass exactly 1 argument.");
+        return 1;
+    }
+}
+
 int iterate(string current_parent_string, string original_hash, string salt)
 //Returns 0 if found, 2 if not found, and 3 if max length is exceded
 {
@@ -33,14 +63,14 @@ int iterate(string current_parent_string, string original_hash, string salt)
 
             //Hash and check
             string new_hash = crypt(current_try, salt);
-            if (strncmp(new_hash, original_hash, 13) == 0)
+            if (!strcmp(new_hash, original_hash))
             {
                 printf("%s\n", current_try);
                 return 0;
             }
 
             //Iterate again, with current try as new parent, and if password is round, return 0
-            if (iterate(current_try, original_hash, salt) == 0)
+            if (!iterate(current_try, original_hash, salt))
             {
                 return 0;
             }
@@ -49,33 +79,4 @@ int iterate(string current_parent_string, string original_hash, string salt)
         return 2;
     }
     return 3;
-}
-
-int main(int argc, string argv[])
-{
-    if (argc == 2)
-    {
-        //Put this in a seperate function when you understand pointers
-        //Get hash and salt
-        char salt[3];
-        string const hash = argv[1];
-        salt[0] = hash[0];
-        salt[1] = hash[1];
-        salt[2] = '\0';
-
-        //Pass to iteration: current parent string, hash, salt
-        if(iterate("", hash, salt) != 0)
-        {
-            //Print status if password not found. If password is found it is printed by the iterate function
-            printf("Password not found in string with length of up to %i characters\nusing the character set \"%s\"\n",
-            max_char_count, characters);
-        }
-        return 0;
-    }
-    else
-    {
-        printf("Error: please pass exactly 1 argument.");
-        return 1;
-    }
-
 }
---
+++ pset2/crack/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+    if(strcmp("na", "a"))
+    {
+        printf("Whaaaat?");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
